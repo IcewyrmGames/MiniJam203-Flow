37ddfb715f04137ecd91b79fa1a5fcb913c4ef6d
C. Hidalgo <andresnotch@gmail.com>
2026-02-01 16:22:14 -0800

    Area that transforms objects in other objects
    - The area sends an event with the object that was touched
    - The object has a script that instantiates a new object and assigns the same position, speed and rotation as the original object.
    - the original object gets immediately deleted to simulate a conversion

---
+++ Assets/Scripts/detect_mana_for_conversion.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public class detect_mana_for_conversion : MonoBehaviour
+{
+    public static event Action<GameObject> OnManaTouch;
+    public GameObject convert_to;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TRIGGERED");
+        Debug.Log(other.name);
+        if (OnManaTouch != null)
+        {
+            String other_name = other.gameObject.name;
+            if (other_name == convert_to.name)
+            {
+                return;
+            }
+            OnManaTouch(other.gameObject);
+        }
+    }
+}
+
